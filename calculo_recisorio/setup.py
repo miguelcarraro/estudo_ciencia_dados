2f8baab277c923baab3a5a72d74aac40c1e972d4
miguelcarraro <miguelcarraromoya@gmail.com>
2025-03-18 23:16:31 -0300

    definindo setup com informações

---
+++ calculo_recisorio/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calculo_recisorio',
+    version='0.0.1',
+    packages=find_packages(),	
+    install_requires=[],
+    description="Pacote para calculo de recisão de contrato de trabalho",
+    author="Miguel Carraro",
+    author_email="miguelcarraromoya@gmail.com",
+    url="https://github.com/miguelcarraro/estudo_ciencia_dados",
+    keywords="recisão, calculo, trabalho"
+)
